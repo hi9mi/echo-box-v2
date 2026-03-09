f4f1964dea540b432e9b346070f5544454e5bce3
Roman <4elove4ek40@gmail.com>
2026-03-09 12:56:37 +0500

    feat: added themes

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,4 +1,4 @@
-import { connectLogger, context, clearStack } from "@reatom/core";
+import { connectLogger, context, clearStack, action } from "@reatom/core";
 import { mount } from "@reatom/jsx";
 import { App } from "./app";
 import "./styles/index.css";
@@ -11,10 +11,20 @@ if (import.meta.env.DEV) {
   rootContext.run(connectLogger);
 }
 
+const setTheme = action(() => {
+  const systemSettingDark = globalThis.matchMedia("(prefers-color-scheme: dark)");
+  if (systemSettingDark.matches) {
+    globalThis.document.documentElement.dataset.theme = "dark";
+  } else {
+    globalThis.document.documentElement.dataset.theme = "light";
+  }
+});
+
 rootContext.run(() => {
   const appEl = document.getElementById("app");
   if (!appEl) {
     throw new Error("No app element");
   }
+  setTheme();
   mount(appEl, <App />);
 });
